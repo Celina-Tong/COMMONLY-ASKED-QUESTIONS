7ea14de58e1917def5b338484cb4c323e52b01e1
Celina Tong <celinanatong@gmail.com>
2023-08-02 21:06:01 -0700

    Initial Commit

---
+++ blossomwords-commonlyaskedquestions/blossomwords_commonlyaskedquestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  blossomwords_commonlyaskedquestionsApp.swift
+//  blossomwords-commonlyaskedquestions
+//
+//  Created by Celina Tong on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct blossomwords_commonlyaskedquestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
